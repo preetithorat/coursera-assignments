5a51eb2bd7d2f99b93fc994a41160860e06a8fe2
Preeti <preetithorat93@gmail.com>
2019-08-25 21:51:45 +0200

    Created helloSpeaker object

--- assignment4-solution/easier/SpeakHello.js
+++ assignment4-solution/easier/SpeakHello.js
@@ -14,9 +14,11 @@
 // STEP 4: Rewrite the 'speak' function such that it is attached to the
 // helloSpeaker object instead of being a standalone function.
 // See Lecture 52, part 2
-function speak(name) {
-  console.log(speakWord + " " + name);
-}
+
+	var helloSpeaker = {};
+	helloSpeaker.speak = function (name) {
+  		console.log(speakWord + " " + name);
+	}
 });
 
 
