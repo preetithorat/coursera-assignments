97de869939136c5e9e670298ca37e21b4580d208
Preeti <preetithorat93@gmail.com>
2019-08-25 21:55:22 +0200

    Upadte speakGoodbye.js
    Wrap the entire contents inside of an IIFE

--- assignment4-solution/easier/SpeakGoodBye.js
+++ assignment4-solution/easier/SpeakGoodBye.js
@@ -11,7 +11,7 @@
 // var byeSpeaker =
 
 // DO NOT attach the speakWord variable to the 'byeSpeaker' object.
-var speakWord = "Good Bye";
+(function () {var speakWord = "Good Bye";
 
 // STEP 8: Rewrite the 'speak' function such that it is attached to the
 // byeSpeaker object instead of being a standalone function.
@@ -19,7 +19,7 @@ var speakWord = "Good Bye";
 function speak(name) {
   console.log(speakWord + " " + name);
 }
-
+})();
 // STEP 9: Expose the 'byeSpeaker' object to the global scope. Name it
 // 'byeSpeaker' on the global scope as well.
 // xxxx.xxxx = byeSpeaker;
